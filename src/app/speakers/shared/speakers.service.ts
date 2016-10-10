f8f41d1d5346241f3f8291e557d39215b82258dc
Lukas Ruebbelke <simpul@gmail.com>
2016-10-10 07:39:38 -0700

    chore: cleaning up

--- src/app/speakers/shared/speakers.service.ts
+++ src/app/speakers/shared/speakers.service.ts
@@ -5,7 +5,7 @@ import { Speaker } from './';
 export class SpeakersService {
   private speakers: Speaker[] = [
     {id: 1, name: 'Luke Ruebbelke', src: 'luke.jpg', bio: 'Co-Founder of @UltimateAngular. Developer. Hacker. Community backer. Author and blogger. Console logger.'},
-    {id: 2, name: 'Ben Lesh', src: 'ben.jpg', bio: '#RxJS 5 project lead. Working at @netflix. I occasionally write about tech and reactive programming. Probably @thejameskyle\'s dad. @_jayphelps lints my code.'},
+    {id: 2, name: 'Todd Motto', src: 'todd.jpg', bio: 'Developer Advocate @Telerik / @ProgressSW. Co-founder @UltimateAngular. Author. Developer Expert @google. Lesser half of @RachaelLPurser. @ken_wheeler advocate.'},
     {id: 3, name: 'Gleb Bahmutov', src: 'gleb.jpg', bio: 'JavaScript ninja, image processing expert, software quality fanatic'},
     {id: 4, name: 'Jeff Whelpley', src: 'jeff.jpg', bio: 'CTO @gethumancom, Google Developer Expert (GDE), Host of @AngularAir, Boston Angular Meetup Co-organizer, Boston College \'00, ♥ Red Sox'},
     {id: 5, name: 'Pascal Precht', src: 'pascal.jpg', bio: 'I like headphones, art, skateboarding and coding. Angular GDE at @Google, @thoughtram co-founder and creator of @5thingsAngular'},
