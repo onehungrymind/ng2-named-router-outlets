fc77f60cf47c02f846679aa83bd07e3e4cda800c
Jonathan Garvey <jon.d.garvey@gmail.com>
2017-10-03 08:58:50 -0700

    auto-highlight selected speaker on component load

--- src/app/speakers/speakers-list/speakers-list.component.ts
+++ src/app/speakers/speakers-list/speakers-list.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Router }  from '@angular/router';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 import { SpeakersService, Speaker } from '../shared';
 
 @Component({
@@ -9,17 +9,26 @@ import { SpeakersService, Speaker } from '../shared';
 })
 export class SpeakersListComponent implements OnInit {
   speakers: Speaker[] = [];
+  selectedSpeaker: number;
 
   constructor(
     private router: Router,
+    private route: ActivatedRoute,
     private service: SpeakersService
   ) { }
 
   ngOnInit() {
     this.speakers = this.service.getSpeakers();
+    this.route.parent.children
+      .find(r => r.outlet === 'bio')
+      .params
+      .subscribe((params: any) => {
+        if (params.id) this.selectedSpeaker = +params.id;
+      });
   }
 
-  showBio(id) {
+  showBio(id: number) {
+    this.selectedSpeaker = id;
     this.router.navigate(['/speakers', {outlets: {'bio': [id]}}]);
   }
 }
