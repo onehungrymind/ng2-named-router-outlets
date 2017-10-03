159d25674a61efb3b970a178b51689163952acde
Jonathan Garvey <jon.d.garvey@gmail.com>
2017-10-03 08:58:18 -0700

    update to @angular/cli from angular-cli

--- src/app/speakers/bio/bio.component.spec.ts
+++ src/app/speakers/bio/bio.component.spec.ts
@@ -2,10 +2,12 @@
 
 import { TestBed, async } from '@angular/core/testing';
 import { BioComponent } from './bio.component';
+import { ActivatedRoute } from '@angular/router';
+import { SpeakersService } from '../shared/speakers.service';
 
 describe('Component: Bio', () => {
   it('should create an instance', () => {
-    let component = new BioComponent();
+    let component = new BioComponent({} as ActivatedRoute, {} as SpeakersService);
     expect(component).toBeTruthy();
   });
 });
--- src/app/speakers/speakers-list/speakers-list.component.spec.ts
+++ src/app/speakers/speakers-list/speakers-list.component.spec.ts
@@ -2,10 +2,12 @@
 
 import { TestBed, async } from '@angular/core/testing';
 import { SpeakersListComponent } from './speakers-list.component';
+import { Router } from '@angular/router';
+import { SpeakersService } from '../shared/speakers.service';
 
 describe('Component: SpeakersList', () => {
   it('should create an instance', () => {
-    let component = new SpeakersListComponent();
+    let component = new SpeakersListComponent({} as Router, {} as SpeakersService);
     expect(component).toBeTruthy();
   });
 });
